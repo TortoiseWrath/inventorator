a726eb7440fe7690aa8b4a098ddd32aa30fbbee4
Sean Gillen <sean@gillen.dev>
2021-04-03 20:10:02 -0500

    make a pong button

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+# TODO: Restrict CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/ping', methods=['GET'])
 def pong():
-    return jsonify('PONG')
+	return jsonify({'data': 'PONG'})
 
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
