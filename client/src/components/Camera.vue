12284743cb559d786e49465521f2d38f92ce44ea
Sean Gillen <sean@gillen.dev>
2021-04-07 16:17:47 -0500

    failed attempt at continuous barcode scanning

--- client/src/components/Camera.vue
+++ client/src/components/Camera.vue
@@ -2,24 +2,31 @@
   <div>
     <video autoplay disablePictureInPicture ref="video"/>
     <button @click="takePhoto">Take Photo</button>
-    <div ref="target"></div>
+    <button @click="startScan">Scan Barcode</button>
+    <div ref="target"/>
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
 import {useToast} from 'vue-toastification';
+import {BrowserMultiFormatReader, NotFoundException} from '@zxing/library';
+import Result from '@zxing/library/esm/core/Result';
+import Exception from '@zxing/library/esm/core/Exception';
 
 export default defineComponent({
   name: 'Camera',
   data() {
     return {
       toast: useToast(),
-      track: {} as MediaStreamTrack,
+      video: {} as HTMLVideoElement,
+      stream: {} as MediaStream,
+      scanningToast: null as string | number | null,
+      scanner: new BrowserMultiFormatReader(),
     };
   },
   mounted() {
-    const video = this.$refs.video as HTMLVideoElement;
+    this.video = this.$refs.video as HTMLVideoElement;
     if (navigator.mediaDevices.getUserMedia) {
       navigator.mediaDevices.getUserMedia({
         video: {
@@ -28,8 +35,8 @@ export default defineComponent({
         },
       })
           .then(stream => {
-            video.srcObject = stream;
-            this.track = stream.getVideoTracks()[0];
+            this.video.srcObject = stream;
+            this.stream = stream;
           })
           .catch(e => console.error(e));
     }
@@ -40,13 +47,13 @@ export default defineComponent({
     },
     barcode(value: string) {
       return new RegExp('^[0-9 ]+$').test(value);
-    }
+    },
   },
   methods: {
     async capturePhoto(): Promise<Blob> {
       // TODO: Webcam fallback for Firefox
       // TODO: Use photoSettings once it's supported somewhere
-      const imageCapture = new ImageCapture(this.track);
+      const imageCapture = new ImageCapture(this.stream.getVideoTracks()[0]);
       return imageCapture.takePhoto();
     },
     async uploadPhoto(blob: Blob) {
@@ -61,13 +68,44 @@ export default defineComponent({
           console.log(`Photo uploaded to: ${json.path}`);
           this.$emit('photo', json.path);
         }
-      } catch (e: any) {
+      } catch (e) {
         console.error(e);
         this.toast.error(e.message);
       }
     },
     takePhoto() {
       this.capturePhoto().then((blob) => this.uploadPhoto(blob));
+    },
+    startScan() {
+      this.scanningToast = this.toast.info('Scanning barcode', {
+        timeout: false,
+        showCloseButtonOnHover: false,
+        onClose: this.stopScan,
+      });
+      this.scanner.decodeFromStream(this.stream, this.video, this.scanCallback);
+    },
+    stopScan() {
+      console.log('STOPPING');
+      this.scanner.stopContinuousDecode();
+      this.video.srcObject = this.stream;
+    },
+    scanCallback(result: Result, err?: Exception) {
+      if (result) {
+        console.log(result);
+        this.toast.warning(result);
+      }
+      if (err) {
+        if (err instanceof NotFoundException) {
+          return; // Keep looking
+        }
+        console.error(err);
+        this.toast.error(err.message);
+      }
+      this.stopScan();
+      if (this.scanningToast != null) {
+        this.toast.dismiss(this.scanningToast);
+        this.scanningToast = null;
+      }
     },
   },
 });
@@ -77,6 +115,7 @@ export default defineComponent({
 video {
   width: 100%;
 }
+
 div {
   max-width: 50%;
 }
